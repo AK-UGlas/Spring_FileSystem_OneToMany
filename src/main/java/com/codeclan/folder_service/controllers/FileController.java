70b0dda5e6f4975ffae09af0dc3c1619a98753a7
Allen_K <allenkelly278@gmail.com>
2021-06-23 00:38:09 +0100

    populate file controller mapping methods

--- src/main/java/com/codeclan/folder_service/controllers/FileController.java
+++ src/main/java/com/codeclan/folder_service/controllers/FileController.java
@@ -1,4 +1,33 @@
 package com.codeclan.folder_service.controllers;
 
+import com.codeclan.folder_service.models.File;
+import com.codeclan.folder_service.repositories.FileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class FileController {
+
+    @Autowired
+    FileRepository fileRepo;
+
+    @GetMapping(value = "/files")
+    public ResponseEntity<List<File>> getAllFiles(){
+        return new ResponseEntity<>(fileRepo.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/files/{id}")
+    public ResponseEntity getPirate(@PathVariable Long id){
+        return new ResponseEntity<>(fileRepo.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/files")
+    public ResponseEntity<File> postPirate(@RequestBody File file) {
+        return new ResponseEntity<>(fileRepo.save(file), HttpStatus.CREATED);
+    }
+
 }
