3d5a7abe972895611c7da5643a8205a5ec6ee3ee
Allen_K <allenkelly278@gmail.com>
2021-06-23 01:02:04 +0100

    populate database with users via DataLoader class

--- src/main/java/com/codeclan/folder_service/components/DataLoader.java
+++ src/main/java/com/codeclan/folder_service/components/DataLoader.java
@@ -1,4 +1,40 @@
 package com.codeclan.folder_service.components;
 
-public class DataLoader {
+import com.codeclan.folder_service.models.User;
+import com.codeclan.folder_service.repositories.FileRepository;
+import com.codeclan.folder_service.repositories.FolderRepository;
+import com.codeclan.folder_service.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataLoader implements ApplicationRunner {
+
+    @Autowired
+    FileRepository fileRepo;
+
+    @Autowired
+    FolderRepository shipRepo;
+
+    @Autowired
+    UserRepository userRepo;
+
+    public DataLoader() {
+    }
+
+    public void run(ApplicationArguments args) {
+        User colin = new User("Colin");
+        User roosa = new User("Roosa");
+        User hannah = new User("Hannah");
+        User sky = new User("Sky");
+
+        userRepo.save(colin);
+        userRepo.save(roosa);
+        userRepo.save(hannah);
+        userRepo.save(sky);
+
+
+    }
 }
